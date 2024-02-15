043299f66d318cd7445b80b19acab72f7cc8d017
LeonelvisR <140525755+LeonelvisR@users.noreply.github.com>
2024-02-15 00:26:57 -0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,10 +12,10 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
+telegram_token = config_yaml["6730484335:AAHENT1zjIhwV9xrFFXbuA_eII18iGA0_C0"]
 openai_api_key = config_yaml["openai_api_key"]
 openai_api_base = config_yaml.get("openai_api_base", None)
-allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
+allowed_telegram_usernames = config_yaml["@leonelvis_bot"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
